0d0013b4b64cf957fd452a3fe806c158fd6781db
Maxel <gigaabait@yandex.ru>
2020-06-19 23:08:32 +0400

    fixed modals

--- script.js
+++ script.js
@@ -11,16 +11,22 @@ const impressum = document.querySelector('.impressum'),
       impressumClose = document.querySelector('.impressum-close');
       impressumClose.addEventListener('click', () => {
           impressum.classList.add('none');
+          document.querySelector('body').style.overflow = 'auto';
       })
       impressumOpen.addEventListener('click', () => {
           impressum.classList.remove('none');
+          impressum.style.top = `${window.pageYOffset}px`;
+          document.querySelector('body').style.overflow = 'hidden';
       })
 const dsgvo= document.querySelector('.dsgvo'),
         dsgvoOpen = document.getElementById('dsgvo-open'),
         dsgvoClose = document.querySelector('.dsgvo-close');
         dsgvoClose.addEventListener('click', () => {
             dsgvo.classList.add('none');
+            document.querySelector('body').style.overflow = 'auto';
             })
             dsgvoOpen.addEventListener('click', () => {
                 dsgvo.classList.remove('none');
+                dsgvo.style.top = `${window.pageYOffset}px`
+                document.querySelector('body').style.overflow = 'hidden';
             })
