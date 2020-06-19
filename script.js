82245051d59498ab2a177109c3befcf2464bcdff
Maxel <gigaabait@yandex.ru>
2020-06-19 22:45:08 +0400

    added modals

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ textHeader.forEach(item => {
         list.classList.toggle('none');
     })
 })
+const impressum = document.querySelector('.impressum'),
+      impressumOpen = document.getElementById('impressum-open'),
+      impressumClose = document.querySelector('.impressum-close');
+      impressumClose.addEventListener('click', () => {
+          impressum.classList.add('none');
+      })
+      impressumOpen.addEventListener('click', () => {
+          impressum.classList.remove('none');
+      })
+const dsgvo= document.querySelector('.dsgvo'),
+        dsgvoOpen = document.getElementById('dsgvo-open'),
+        dsgvoClose = document.querySelector('.dsgvo-close');
+        dsgvoClose.addEventListener('click', () => {
+            dsgvo.classList.add('none');
+            })
+            dsgvoOpen.addEventListener('click', () => {
+                dsgvo.classList.remove('none');
+            })
